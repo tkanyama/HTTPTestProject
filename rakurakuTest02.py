7b4049e80a438cde21375330f80c7a6f36793844
tkanyama <toshi.kanyama@gmail.com>
2022-02-06 08:25:05 +0900

    2021/11/9-3

--- rakurakuTest02.py
+++ rakurakuTest02.py
@@ -1,43 +1,37 @@
 import requests
 import urllib.parse
+import json
+from io import BytesIO
 
-file_path_name = '/Users/tkanyama/Google ドライブ/Work/情報システムWG/経理課関連/顧客データ/顧客20211112.csv'
+file_path_name = '/Users/tkanyama/マイドライブ/Work/情報システムWG/経理課関連/VBAサンプルプログラム3_2/サンプルデータ/顧客データ/顧客データ1.csv'
 file_path_name2 = urllib.parse.quote(file_path_name)
 #POSTするURL設定
-post_url = "https://ta.eco-serv.jp/gbrc/api/v1/customers/imports"
+post_url = "https://rbthyme.eco-serv.jp/gbrc/api/v1/customers/imports"
 
 #POSTするデータ設定
-
-# data = {"json":("",{"ispdatenfo": 0,
-#             "updateBlank": 2,
-#             "importProcessName": "顧客データ取込サンプル",
-#             "skipFirst": 1,
-#             "settingId": 5
-#          },"application/json")}
-# json_data = { "col1": "val1",
-#               "col2": "val2",
-#               "col3": "val3",
-#               "col4": data
-#             }
-#POSTするファイルsの読込
-# files = { "files[0]": ("顧客20211112.csv",open(file_path_name, 'r', encoding='shift_jis'),'text/csv') }#ヘッダー設定
-files = { "json":("",{"ispdatenfo": "0",
-            "updateBlank": "2",
+json1 = {
+            "isUpdateInfo": 1,
+            "updateBlank": 2,
             "importProcessName": "顧客データ取込サンプル",
-            "skipFirst": "1",
-            "settingId": "5"
-         },"application/json"),
-    "file": ("顧客20211112.csv",open(file_path_name, 'r', encoding='shift_jis'),'text/csv') }#ヘッダー設定
-headers = {
-            'X-WB-apitoken': 'AZsH5M76XPXASjHAkEcIGOKtfjUL46CXrpadiQJfh9srDghuGLp6hxXC0DTfegbK'
-            }
+            "skipFirst": 1 ,
+            "settingId": 5
+         }
+json2 = BytesIO(json.dumps(json1,ensure_ascii = False).encode('utf-8'))
+data={ "json":json2 }
 
+#POSTするファイルsの読込
+filepath = "/Users/tkanyama/マイドライブ/Work/情報システムWG/経理課関連/VBAサンプルプログラム3_2/サンプルデータ/顧客データ/顧客データ1.csv"
+files = { "files[0]": ("顧客データ1.csv",open(file_path_name, 'rb'),"text/csv; charset=Windows-31J") }#ヘッダー設定
+
+headers = {
+            "X-WB-apitoken": "AZsH5M76XPXASjHAkEcIGOKtfjUL46CXrpadiQJfh9srDghuGLp6hxXC0DTfegbK"
+            }
 #POST送信
 response = requests.post(
     post_url,
-    # data = data,
+    data = data,
     files = files,
     headers = headers)
 
-print(response)
+print(response.text)
 print("OK")
