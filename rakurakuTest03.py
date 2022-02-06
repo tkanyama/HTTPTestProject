f1f6b1562b5df10ab3230b16294fc9da8b6f5881
tkanyama <toshi.kanyama@gmail.com>
2022-02-06 17:31:19 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	rakurakuTest02.py
    #	rakurakuTest03.py

--- rakurakuTest03.py
+++ rakurakuTest03.py
@@ -2,9 +2,20 @@ import requests
 import urllib.parse
 import json
 from io import BytesIO
+from tkinter import filedialog
+import os
+typ1 = [('zipファイル','*.zip')]
+dir1 = '/Users/tkanyama/マイドライブ/Work/情報システムWG/経理課関連/VBAサンプルプログラム3_2/サンプルデータ/帳票データ'
+file_path_name1 = filedialog.askopenfilename(filetypes = typ1, initialdir = dir1)
+filename1 = os.path.basename(file_path_name1)
 
-file_path_name1 = '/Users/tkanyama/マイドライブ/Work/情報システムWG/経理課関連/VBAサンプルプログラム3_2/サンプルデータ/帳票データ/テスト用データ.zip'
-file_path_name2 = '/Users/tkanyama/マイドライブ/Work/情報システムWG/経理課関連/VBAサンプルプログラム3_2/サンプルデータ/帳票データ/帳票データ_Zip.csv'
+typ2 = [('csvファイル','*.csv')]
+dir2 = '/Users/tkanyama/マイドライブ/Work/情報システムWG/経理課関連/VBAサンプルプログラム3_2/サンプルデータ/帳票データ'
+file_path_name2 = filedialog.askopenfilename(filetypes = typ2, initialdir = dir2)
+filename2 = os.path.basename(file_path_name2)
+
+# file_path_name1 = '/Users/tkanyama/マイドライブ/Work/情報システムWG/経理課関連/VBAサンプルプログラム3_2/サンプルデータ/帳票データ/テスト用データ.zip'
+# file_path_name2 = '/Users/tkanyama/マイドライブ/Work/情報システムWG/経理課関連/VBAサンプルプログラム3_2/サンプルデータ/帳票データ/帳票データ_Zip.csv'
 #POSTするURL設定
 post_url = "https://rbthyme.eco-serv.jp/gbrc/api/v1/reports/imports"
 
@@ -25,9 +36,13 @@ with open(file_path_name2, 'r' ,encoding='shift_jis') as f:
 readdata = BytesIO(csvdata.encode('shift_jis'))
 #POSTするファイルの読込
 # filepath = "/Users/tkanyama/マイドライブ/Work/情報システムWG/経理課関連/VBAサンプルプログラム3_2/サンプルデータ/顧客データ/顧客データ1.csv"
+# files = {
+#     "files[0]": ("テスト用データ.zip", open(file_path_name1, 'rb'), "application/zip"),
+#     "files[1]": ("帳票データ_Zip.csv", readdata, "text/csv; charset=Windows-31J"),
+# }
 files = {
-    "files[0]": ("テスト用データ.zip", open(file_path_name1, 'rb'), "application/zip"),
-    "files[1]": ("帳票データ_Zip.csv", readdata, "text/csv; charset=Windows-31J"),
+    "files[0]": (filename1, open(file_path_name1, 'rb'), ""),
+    "files[1]": (filename2, readdata, ""),
 }
 
 #ヘッダー設定
