7554adeb6500768c135bb5ac0d664863193cf95d
tkanyama <toshi.kanyama@gmail.com>
2022-02-06 09:34:34 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	rakurakuTest02.py

--- rakurakuTest03.py
+++ rakurakuTest03.py
@@ -20,10 +20,13 @@ json1 = {
 json2 = BytesIO(json.dumps(json1,ensure_ascii = False).encode('utf-8'))
 data={ "json":json2 }
 
+with open(file_path_name2, 'r' ,encoding='shift_jis') as f:
+    csvdata = f.read()
+readdata = BytesIO(csvdata.encode('shift_jis'))
 #POSTするファイルsの読込
 # filepath = "/Users/tkanyama/マイドライブ/Work/情報システムWG/経理課関連/VBAサンプルプログラム3_2/サンプルデータ/顧客データ/顧客データ1.csv"
 files = {
-    "files[1]": ("帳票データ_Zip.csv", open(file_path_name2, 'rb'), "text/csv; charset=Windows-31J"),
+    "files[1]": ("帳票データ_Zip.csv", readdata, "text/csv; charset=Windows-31J"),
     "files[0]": ("テスト用データ.zip",open(file_path_name, 'rb'),"application/zip")
 }#ヘッダー設定
 
