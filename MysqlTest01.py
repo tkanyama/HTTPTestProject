4906b26482f686cdc083bed4af7063191f71f4d4
Toshiyuki Kanyama <toshi.kanyama@gmail.com>
2022-05-26 10:57:18 +0900

    2022/05/26 大幅修正

--- MysqlTest01.py
+++ MysqlTest01.py
@@ -9,19 +9,21 @@ from mysql.connector import errorcode
 
 class MySqlAPI():
     def __init__(self, *args, **kwargs):
-        self.config = {
-            'user': 'tkanyama',
-            'password': 'momo1momo1',
-            'host': '192.168.32.81',
-            'database': 'world'
-        }
+        self.user = 'tkanyama'
+        self.password = 'momo1momo1'
+        self.host = '192.168.32.81'
 
-    def set_config(self, user='tkanyama', password='momo1momo1', host='192.168.32.81', database='world'):
+        self.set_config(
+            user=self.user, password=self.password, host=self.host)
+
+    def set_config(self, user='tkanyama', password='momo1momo1', host='192.168.32.81'):
+        self.user = user
+        self.password = password
+        self.host = host
         self.config = {
-            'user': user,
-            'password': password,
-            'host': host,
-            'database': database
+            'user': self.user,
+            'password': self.password,
+            'host': self.host
         }
 
     def connect(self):
@@ -41,7 +43,27 @@ class MySqlAPI():
         # rows = cur.fetchall()
         return rows
 
-    def fieldname(self, tablename='city'):
+    def databasenames(self):
+        # データベース一覧を取得
+        cur = self.conn.cursor()
+        cur.execute("SHOW DATABASES")
+        rows = cur.fetchall()
+        return rows
+
+    def tablenames(self):
+        # データベース一覧を取得
+        cur = self.conn.cursor()
+        cur.execute("SHOW TABLES")
+        rows = cur.fetchall()
+        return rows
+
+    def changedatabase(self, database='world'):
+        cur = self.conn.cursor()
+        cur.execute('USE {}'.format(database))
+        # rows = cur.fetchall()
+        # return rows
+
+    def fieldnames(self, tablename='city'):
         # フィールド一覧を取得
         cur = self.conn.cursor()
         cur.execute("DESC " + tablename)
@@ -53,33 +75,51 @@ class MySqlAPI():
 
 
 if __name__ == '__main__':
+
     my1 = MySqlAPI()
+
     my1.set_config(user='tkanyama',
                    password='momo1momo1',
-                   host='192.168.32.81',
-                   database='world'
+                   host='192.168.32.81'
                    )
     if my1.connect():
-        sql_str1 = "SELECT * FROM city WHERE CountryCode = 'JPN' AND District='Osaka'"
-        rows = my1.select(sql_str=sql_str1)
-        print(rows.__len__())
-        fields = my1.fieldname(tablename='city')
-        n = fields.__len__()
-        i = 0
-        for field in fields:
-            i += 1
-            print(field[0], end='')
-            if i < n:
-                print(' , ', end='')
-        print('')
 
-        for row in rows:
-            i = 0
-            for field in fields:
-                i += 1
-                print(row[field[0]], end='')
-                if i < n:
-                    print(' , ', end='')
+        databases = my1.databasenames()
+        for databese in databases:
+            if databese[0] != 'information_schema' and databese[0] != 'mysql' and databese[0] != 'performance_schema':
+                print("***********************")
+                print("  databesename = {}".format(databese[0]))
+                print("***********************")
+                print("")
+                my1.changedatabase(databese[0])
+                tablenames = my1.tablenames()
+                n = len(tablenames)
 
-            print('')
+                for tabelname in tablenames:
+                    print("***********************")
+                    print("  tablename = {}".format(tabelname[0]))
+                    print("***********************")
+                    fields = my1.fieldnames(tablename=tabelname[0])
+                    n = fields.__len__()
+                    i = 0
+                    for field in fields:
+                        i += 1
+                        print(field[0], end='')
+                        if i < n:
+                            print(' , ', end='')
+                    print('')
+
+                    sql_str1 = "SELECT * FROM {} LIMIT 10".format(tabelname[0])
+                    rows = my1.select(sql_str=sql_str1)
+                    for row in rows:
+                        i = 0
+                        for field in fields:
+                            i += 1
+                            print(row[field[0]], end='')
+                            if i < n:
+                                print(' , ', end='')
+
+                        print('')
+
+                    print('')
         my1.close()
