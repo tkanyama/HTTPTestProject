609db0bf624f6499f1f4ebca92ac1e4e89d13840
tkanyama <toshi.kanyama@gmail.com>
2022-10-12 16:01:57 +0900

    2022/10/12-2

--- FMDataAPI.py
+++ FMDataAPI.py
@@ -4,8 +4,10 @@ import json
 import time
 import datetime
 import os.path
+import base64
+# print(base64.b64encode('こんにちは'.encode()))
 
-class SalesForceAPI():
+class FMDataAPI():
 
     def __init__(self, *args, **kwargs):
         self.tokenReset()
@@ -26,57 +28,121 @@ class SalesForceAPI():
         f = open(path, 'r')
         datajson = json.loads(f.read())
         f.close()
-        URL1 = datajson['URL1']
-        URL2 = datajson['URL2']
-        URL3 = datajson['URL3']
-        ID = datajson['ID']
-        SECRET = datajson['SECRET']
+        self.serverAddress = "192.168.0.171"
+        self.databaseName = "楽楽明細"
+        self.layoutName = "顧客リスト"
+        RecId = 125
+        self.URL1 = datajson['URL1']
+        self.URL2 = datajson['URL2']
+        self.URL3 = datajson['URL3']
+
+        URL = self.URL1.format(self.serverAddress, self.databaseName)
+        # ID = datajson['ID']
+        # SECRET = datajson['SECRET']
         USER = datajson['USER']
         PASSWORD = datajson['PASSWORD']
+        idpw = base64.b64encode((USER + ':' + PASSWORD).encode()).decode()
+        # idpw = datajson['idpw']
 
-        data = {'client_id': ID,
-                'client_secret': SECRET,
-                'username': USER,
-                'password': PASSWORD,
-                'grant_type': 'password'
-                }
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Basic ' + idpw
+            }
+        
+        data = {}
+        # data = {'client_id': ID,
+        #         'client_secret': SECRET,
+        #         'username': USER,
+        #         'password': PASSWORD,
+        #         'grant_type': 'password'
+        #         }
         # POST送信
         response = requests.post(
             URL,
-            data=data
+            data=data,
+            headers=headers,
+            verify=False
         )
         self.res = response.text
 
         json_dict = json.loads(self.res)
-        self.access_token = json_dict['access_token']
-        self.instance_url = json_dict['instance_url']
-        self.id = json_dict['id']
-        self.token_type = json_dict['token_type']
-        self.issued_at = json_dict['issued_at']
-        self.signature = json_dict['signature']
-        self.settime = datetime.datetime.now()
-        # print(self.access_token)
+        self.access_token = json_dict['response']['token']
+        # self.access_token = response1['token']
 
-    def dataread(self):
+        # self.instance_url = json_dict['instance_url']
+        # self.id = json_dict['id']
+        # self.token_type = json_dict['token_type']
+        # self.issued_at = json_dict['issued_at']
+        # self.signature = json_dict['signature']
+        self.settime = datetime.datetime.now()
+        print(self.access_token)
+
+    def datareadbyID(self):
         if datetime.datetime.now() - self.settime > datetime.timedelta(minutes=5):
             self.tokenReset()
-        query = 'SELECT Name__c,Address1__c,Section_Name__c,Name FROM Customer_Data1__c WHERE Name__c LIKE \'東海カラー%\''
-        url = self.instance_url + "/services/data/v54.0/query/?q=" + query
-        headers = {'Authorization': self.token_type + ' ' + self.access_token}
+        
+        url = self.URL2.format(self.serverAddress, self.databaseName, self.layoutName, 125)
+
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer' + ' ' + self.access_token
+        }
+        
+        data = {}
+
         response = requests.get(
             url,
-            headers=headers
+            headers=headers,
+            data=data,
+            verify=False
+        )
+        res = response.text
+        return res
+
+    def datareadbyFind(self):
+        if datetime.datetime.now() - self.settime > datetime.timedelta(minutes=5):
+            self.tokenReset()
+        
+        url = self.URL3.format(self.serverAddress, self.databaseName, self.layoutName)
+        
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer' + ' ' + self.access_token
+        }
+        
+        json1 = {
+            "query":[
+                    {"更新日時": "04/01/2021...03/31/2023","担当部署名": "総務課" }
+                    ],
+            "sort": [
+                    {"fieldName": "更新日時","sortOrder": "ascend" }
+                    ]
+        }
+
+        response = requests.post(
+            url,
+            headers=headers,
+            json=json1,
+            verify=False
         )
         res = response.text
         return res
 
 if __name__ == '__main__':
-    sf = SalesForceAPI()
+    sf = FMDataAPI()
     print(sf.access_token)
-    res = sf.dataread()
-    print(res)
-    json_dict = json.loads(res)
-    n = int(json_dict['totalSize'])
+    res1 = sf.datareadbyID()
+    print(res1)
+    json_dict = json.loads(res1)
+    n = int(json_dict['response']['dataInfo']['returnedCount'])
     for i in range(n):
-        name = json_dict['records'][i]['Name__c']
+        name = json_dict['response']['data'][i]['fieldData']['顧客名']
+        print(name)
+
+    res2 = sf.datareadbyFind()
+    print(res2)
+    json_dict = json.loads(res2)
+    n = int(json_dict['response']['dataInfo']['returnedCount'])
+    for i in range(n):
+        name = json_dict['response']['data'][i]['fieldData']['顧客名']
         print(name)
