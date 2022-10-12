7e64b687f7c238d04c55301f6629baf97fae3242
tkanyama <toshi.kanyama@gmail.com>
2022-10-12 18:43:27 +0900

    2022/10/12-3

--- FMDataAPI.py
+++ FMDataAPI.py
@@ -10,19 +10,9 @@ import base64
 class FMDataAPI():
 
     def __init__(self, *args, **kwargs):
-        self.tokenReset()
+        self.RMDataAPILogin()
 
-    def tokenReset(self):
-        # f = open('/Users/tkanyama/マイドライブ/init.txt', 'r')
-        # path = os.path.expanduser('~/init.txt')
-        # f = open(path, 'r')
-        # datalist = f.readlines()
-        # f.close()
-        # URL = datalist[0].rstrip('\n')
-        # ID = datalist[1].rstrip('\n')
-        # SECRET= datalist[2].rstrip('\n')
-        # USER= datalist[3].rstrip('\n')
-        # PASSWORD = datalist[4].rstrip('\n')
+    def RMDataAPILogin(self):
 
         path = os.path.expanduser('~/init2.json')
         f = open(path, 'r')
@@ -31,55 +21,64 @@ class FMDataAPI():
         self.serverAddress = "192.168.0.171"
         self.databaseName = "楽楽明細"
         self.layoutName = "顧客リスト"
-        RecId = 125
+        # RecId = 125
         self.URL1 = datajson['URL1']
         self.URL2 = datajson['URL2']
         self.URL3 = datajson['URL3']
 
         URL = self.URL1.format(self.serverAddress, self.databaseName)
-        # ID = datajson['ID']
-        # SECRET = datajson['SECRET']
-        USER = datajson['USER']
-        PASSWORD = datajson['PASSWORD']
-        idpw = base64.b64encode((USER + ':' + PASSWORD).encode()).decode()
-        # idpw = datajson['idpw']
 
+        self.USER = datajson['USER']
+        self.PASSWORD = datajson['PASSWORD']
+        self.idpw = base64.b64encode((self.USER + ':' + self.PASSWORD).encode()).decode()
+ 
         headers = {
             'Content-Type': 'application/json',
-            'Authorization': 'Basic ' + idpw
+            'Authorization': 'Basic ' + self.idpw
             }
         
-        data = {}
-        # data = {'client_id': ID,
-        #         'client_secret': SECRET,
-        #         'username': USER,
-        #         'password': PASSWORD,
-        #         'grant_type': 'password'
-        #         }
-        # POST送信
+       # POST送信
         response = requests.post(
             URL,
-            data=data,
             headers=headers,
+            json = {},
             verify=False
         )
-        self.res = response.text
 
-        json_dict = json.loads(self.res)
-        self.access_token = json_dict['response']['token']
-        # self.access_token = response1['token']
+        if response.status_code==200:
+            res = response.text
+            json_dict = json.loads(res)
+            self.access_token = json_dict['response']['token']
+            self.settime = datetime.datetime.now()
+        else:
+            res = ""
+            self.access_token = ""
+            
+    def RMDataAPILogOut(self):
 
-        # self.instance_url = json_dict['instance_url']
-        # self.id = json_dict['id']
-        # self.token_type = json_dict['token_type']
-        # self.issued_at = json_dict['issued_at']
-        # self.signature = json_dict['signature']
-        self.settime = datetime.datetime.now()
-        print(self.access_token)
+        URL = self.URL1.format(self.serverAddress, self.databaseName) + '/' + self.access_token
+ 
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Basic ' + self.idpw
+            }
+        
+       # POST送信
+        response = requests.delete(
+            URL,
+            headers=headers,
+            json = {},
+            verify=False
+        )
+        if response.status_code==200:
+            res = response.text
+        else:
+            res = ""
+        return res
 
     def datareadbyID(self):
         if datetime.datetime.now() - self.settime > datetime.timedelta(minutes=5):
-            self.tokenReset()
+            self.RMDataAPILogin()
         
         url = self.URL2.format(self.serverAddress, self.databaseName, self.layoutName, 125)
 
@@ -88,20 +87,20 @@ class FMDataAPI():
             'Authorization': 'Bearer' + ' ' + self.access_token
         }
         
-        data = {}
-
         response = requests.get(
             url,
             headers=headers,
-            data=data,
             verify=False
         )
-        res = response.text
+        if response.status_code==200:
+            res = response.text
+        else:
+            res = ""
         return res
 
     def datareadbyFind(self):
         if datetime.datetime.now() - self.settime > datetime.timedelta(minutes=5):
-            self.tokenReset()
+            self.RMDataAPILogin()
         
         url = self.URL3.format(self.serverAddress, self.databaseName, self.layoutName)
         
@@ -125,24 +124,33 @@ class FMDataAPI():
             json=json1,
             verify=False
         )
-        res = response.text
+        if response.status_code==200:
+            res = response.text
+        else:
+            res = ""
         return res
 
 if __name__ == '__main__':
     sf = FMDataAPI()
-    print(sf.access_token)
+    # print(sf.access_token)
     res1 = sf.datareadbyID()
-    print(res1)
-    json_dict = json.loads(res1)
-    n = int(json_dict['response']['dataInfo']['returnedCount'])
-    for i in range(n):
-        name = json_dict['response']['data'][i]['fieldData']['顧客名']
-        print(name)
+    if res1 != "":
+        # print(res1)
+        json_dict = json.loads(res1)
+        
+        n = int(json_dict['response']['dataInfo']['returnedCount'])
+        for i in range(n):
+            name = json_dict['response']['data'][i]['fieldData']['顧客名']
+            print(name)
 
     res2 = sf.datareadbyFind()
-    print(res2)
-    json_dict = json.loads(res2)
-    n = int(json_dict['response']['dataInfo']['returnedCount'])
-    for i in range(n):
-        name = json_dict['response']['data'][i]['fieldData']['顧客名']
-        print(name)
+    if res2 != "":
+        # print(res2)
+        json_dict = json.loads(res2)
+        n = int(json_dict['response']['dataInfo']['returnedCount'])
+        for i in range(n):
+            name = json_dict['response']['data'][i]['fieldData']['顧客名']
+            print(name)
+
+    res3 = sf.RMDataAPILogOut()
+    print(res3)
