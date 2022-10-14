945bd48797c86f90616d879380c1e1de4e166ac3
tkanyama <toshi.kanyama@gmail.com>
2022-10-14 11:35:36 +0900

    2022/10/14-1

---
+++ FMDataAPI2.py
@@ -0,0 +1,315 @@
+from multiprocessing.resource_sharer import stop
+import requests
+import urllib.parse
+import json
+import time
+import datetime
+import os.path
+import base64
+import sys
+# print(base64.b64encode('こんにちは'.encode()))
+
+class FMDataAPI():
+
+    def __init__(self, serverAddress, databaseName, layoutName):
+        if not self.RMDataAPILogin(serverAddress, databaseName, layoutName):            
+            sys.exit()
+
+
+
+    def RMDataAPILogin(self, serverAddress, databaseName, layoutName):
+
+        # IDとパスワードをJsonファイルから読み込む
+        path = os.path.expanduser('~/init2.json')
+        f = open(path, 'r')
+        datajson = json.loads(f.read())
+        f.close()
+        self.USER = datajson['USER']
+        self.PASSWORD = datajson['PASSWORD']
+        # "ID:Password"をBase64にエンコード（binary）し、さらに、文字列にデコード
+        self.idpw = base64.b64encode((self.USER + ':' + self.PASSWORD).encode()).decode()
+
+        # self.serverAddress = "192.168.0.171"
+        # self.databaseName = "楽楽明細"
+        # self.layoutName = "顧客リスト"
+
+        self.serverAddress = serverAddress
+        self.databaseName = databaseName
+        self.layoutName = layoutName
+        
+        self.URL1 = "https://{}/fmi/data/vLatest/databases/{}/sessions"
+        self.URL2 = "https://{}/fmi/data/vLatest/databases/{}//layouts/{}/records/{}"
+        self.URL3 = "https://{}/fmi/data/vLatest/databases/{}//layouts/{}/_find"
+
+        self.URL4 = "https://{}/fmi/data/vLatest/productInfo".format(self.serverAddress)
+        response = requests.get(
+            self.URL4,
+            headers={},
+            json = {},
+            verify=False
+        )
+        if response.status_code == 200:
+            json_dict = json.loads(response.text)
+            self.dateFormat = json_dict['response']["productInfo"]['dateFormat']
+            self.timeStampFormat = json_dict['response']["productInfo"]['timeStampFormat']
+            self.timeFormat = json_dict['response']["productInfo"]['timeFormat']
+
+           
+
+        # RestAPIで通信可能なデータベース名を取得
+        self.databases = self.readDatabaseName()
+
+        URL = self.URL1.format(self.serverAddress, self.databaseName)
+
+ 
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Basic ' + self.idpw
+            }
+        
+       # POST送信
+        response = requests.post(
+            URL,
+            headers=headers,
+            json = {},
+            verify=False
+        )
+
+        if response.status_code==200:
+            res = response.text
+            json_dict = json.loads(res)
+            self.access_token = json_dict['response']['token']
+            self.settime = datetime.datetime.now()
+
+            # データベースに含まれるスクリプト名をすべて取得
+            self.scripts = self.readScriptName()
+            # データベースに含まれるレオアウト名をすべて取得
+            self.layouts = self.readLyoutName()
+            
+            # 指定されたデータベースやレイアウトが実際にあるかどうかチェックする
+            if (self.databaseName in self.databases) and (self.layoutName in self.layouts):
+                return True
+            else:
+                print("データベースまたはレイアウトが存在しません!!\n 終了します!!")
+                return False
+
+        else:
+            res = ""
+            self.access_token = ""
+            return False
+            
+    def RMDataAPILogOut(self):
+
+        URL = self.URL1.format(self.serverAddress, self.databaseName) + '/' + self.access_token
+ 
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Basic ' + self.idpw
+            }
+        
+       # DELETE送信
+        response = requests.delete(
+            URL,
+            headers=headers,
+            json = {},
+            verify=False
+        )
+        if response.status_code==200:
+            res = response.text
+        else:
+            res = ""
+        return res
+
+    def readDatabaseName(self):
+
+        self.URL5 = "https://{}/fmi/data/vLatest/databases".format(self.serverAddress)
+
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Basic ' + self.idpw
+            }
+        response = requests.get(
+            self.URL5,
+            headers=headers,
+            json = {},
+            verify=False
+        )
+        names = []
+        if response.status_code == 200:
+            res = response.text
+            json_dict = json.loads(res)
+            databases = json_dict['response']["databases"]
+            for database in databases:
+                names.append(database['name'])
+                print(database['name'])
+  
+        return names
+
+    def readScriptName(self):
+
+        self.URL6 = "https://{}/fmi/data/vLatest/databases/{}/scripts".format(self.serverAddress, self.databaseName)
+
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer' + ' ' + self.access_token
+        }
+
+        response = requests.get(
+            self.URL6,
+            headers=headers,
+            json = {},
+            verify=False
+        )
+        names = []
+        if response.status_code == 200:
+            res = response.text
+            json_dict = json.loads(res)
+            scripts = json_dict['response']["scripts"]
+            for script in scripts:
+                names.append(script['name'])
+                print(script['name'])
+  
+        return names
+
+    def readLyoutName(self):
+
+        self.URL7 = "https://{}/fmi/data/vLatest/databases/{}/layouts".format(self.serverAddress, self.databaseName)
+
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer' + ' ' + self.access_token
+        }
+
+        response = requests.get(
+            self.URL7,
+            headers=headers,
+            json = {},
+            verify=False
+        )
+        names = []
+        if response.status_code == 200:
+            res = response.text
+            json_dict = json.loads(res)
+            layouts = json_dict['response']["layouts"]
+            for layout in layouts:
+                names.append(layout['name'])
+                print(layout['name'])
+  
+        return names
+
+
+    def datareadbyID(self):
+        if datetime.datetime.now() - self.settime > datetime.timedelta(minutes=5):
+            self.RMDataAPILogin()
+        
+        url = self.URL2.format(self.serverAddress, self.databaseName, self.layoutName, 160)
+
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer' + ' ' + self.access_token
+        }
+        
+        response = requests.get(
+            url,
+            headers=headers,
+            verify=False
+        )
+        if response.status_code==200:
+            res = response.text
+        else:
+            res = ""
+        return res
+
+    def datareadbyFind(self):
+        if datetime.datetime.now() - self.settime > datetime.timedelta(minutes=5):
+            self.RMDataAPILogin()
+        
+        url = self.URL3.format(self.serverAddress, self.databaseName, self.layoutName)
+        
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer' + ' ' + self.access_token
+        }
+        
+        json1 = {
+            "query":[
+                    {"作成日": "09/01/1971","分類": "役員会議事録" }
+                    ],
+            "sort": [
+                    {"fieldName": "作成日","sortOrder": "ascend" }
+                    ]
+        }
+
+        response = requests.post(
+            url,
+            headers=headers,
+            json=json1,
+            verify=False
+        )
+        if response.status_code==200:
+            res = response.text
+        else:
+            res = ""
+        return res
+
+    def roadPDF(self, url):
+
+        # headers = {
+        #     'Content-Type': 'application/pdf',
+        #     'Authorization': 'Bearer' + ' ' + self.access_token
+        # }
+        response = requests.get(url,
+                headers={},
+                stream=True,
+                verify=False)
+
+        if response.status_code == 200:
+            file = open("/Users/kanyama/test1.pdf","wb")
+            #iter_content()メソッドでWebファイルのデータを渡す
+            c = 0
+            for chunk in response.iter_content(100000):
+                #ファイル書込
+                file.write(chunk)
+                c += 1
+                
+            #ファイルを閉じる
+            file.close()
+            print("Count = {} 回".format(c))
+
+if __name__ == '__main__':
+
+    serverAddress = "192.168.0.171"
+    databaseName = "総務課文書管理"
+    layoutName = "文書単票"
+    sf = FMDataAPI(serverAddress, databaseName ,layoutName)
+
+    databases = sf.readDatabaseName()
+    print(sf.access_token)
+    res1 = sf.datareadbyID()
+    if res1 != "":
+        # print(res1)
+        json_dict = json.loads(res1)
+        
+        n = int(json_dict['response']['dataInfo']['returnedCount'])
+        for i in range(n):
+            name = json_dict['response']['data'][i]['fieldData']['PDF']
+            print(name)
+            # url = name[0:name.find('?RCType')]
+            url = name
+            sf.roadPDF(url)
+            keys = json_dict['response']['data'][i]['fieldData'].keys()
+            print(keys)
+            for key in keys:
+                print(key , ':' ,json_dict['response']['data'][i]['fieldData'][key])
+
+    res2 = sf.datareadbyFind()
+    if res2 != "":
+        # print(res2)
+        json_dict = json.loads(res2)
+        n = int(json_dict['response']['dataInfo']['returnedCount'])
+        for i in range(n):
+            name = json_dict['response']['data'][i]['fieldData']['PDF']
+            print(name)
+
+    res3 = sf.RMDataAPILogOut()
+    print(res3)
---
+++ FMRestAPI.py
@@ -0,0 +1,76 @@
+import json
+import os.path
+import fmrest
+
+
+def savePDF(url, fms , filename):
+    name, type_, length, response = fms.fetch_file(url, stream=True)
+    with open(filename, 'wb') as file_:
+        for chunk in response.iter_content(chunk_size=100000): 
+            if chunk:
+                file_.write(chunk)
+
+if __name__ == '__main__':
+# IDとパスワードをJsonファイルから読み込む
+    path = os.path.expanduser('~/init2.json')
+    f = open(path, 'r')
+    datajson = json.loads(f.read())
+    f.close()
+    USER = datajson['USER']
+    PASSWORD = datajson['PASSWORD']
+    serverAddress = "192.168.0.171"
+    databaseName = "総務課文書管理"
+    layoutName = "文書単票"
+   
+    fms = fmrest.Server("https://{}".format(serverAddress),
+                user=USER,
+                password=PASSWORD,
+                database=databaseName,
+                layout=layoutName,
+                api_version='vLatest',
+                verify_ssl=False
+                )
+    fms.login()
+    print(fms)
+    databases = fms.get_databases()
+    lyouts = fms.get_layouts()
+
+    res1 = fms.get_record(160)
+    dic = res1.to_dict()
+    keys = res1.keys()
+    v1 = res1.values()
+    
+    for key in keys:
+        print(key, ":",dic[key])
+        if key == "PDF":
+            savePDF(dic[key], fms , "/Users/kanyama/test2.pdf")
+
+    res2 = fms.get_records(limit=2)
+    n=0
+    for res in res2:
+        dic = res.to_dict()
+        keys = res.keys()
+        v1 = res.values()
+        for key in keys:
+            print(key, ":",dic[key])
+            if key == "PDF":
+                n += 1
+                savePDF(dic[key], fms , "/Users/kanyama/PDF/test{}.pdf".format(n))
+
+    find_query = [
+                {"作成日": "09/01/1971","分類": "役員会議事録" }
+                ]
+
+    order_by = [
+        {"fieldName": "作成日","sortOrder": "ascend" }
+        ] 
+
+    res3 = fms.find(find_query, sort=order_by)
+    for res in res3:
+        dic = res.to_dict()
+        keys = res.keys()
+        v1 = res.values()
+        for key in keys:
+            print(key, ":",dic[key])
+
+    fms.logout()
