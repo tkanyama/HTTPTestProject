6d1372be2a587a24d267ce0f432425384ce2d6a9
tkanyama <toshi.kanyama@gmail.com>
2022-10-25 16:13:31 +0900

    2022/10/25-2

--- SalesForceAPI.py
+++ SalesForceAPI.py
@@ -1,3 +1,4 @@
+from email import header
 import requests
 import urllib.parse
 import json
@@ -54,16 +55,18 @@ class SalesForceAPI():
         self.signature = json_dict['signature']
         self.settime = datetime.datetime.now()
         # print(self.access_token)
+        self.url = ""
+        self.resourse = {}
 
     def dataread(self):
         if datetime.datetime.now() - self.settime > datetime.timedelta(minutes=5):
             self.tokenReset()
         query = 'SELECT Name__c,Address1__c,Section_Name__c,Name FROM Customer_Data1__c WHERE Name__c LIKE \'東海カラー%\''
-        url = self.instance_url + "/services/data/v54.0/query/?q=" + query
-        headers = {'Authorization': self.token_type + ' ' + self.access_token}
+        # url = self.instance_url + self.url + "/query/?q=" + query
+        # headers = {'Authorization': self.token_type + ' ' + self.access_token}
         response = requests.get(
-            url,
-            headers=headers
+            self.instance_url + self.url + "/query/?q=" + query,
+            headers={'Authorization': self.token_type + ' ' + self.access_token}
         )
         res = response.text
         return res
@@ -72,22 +75,90 @@ class SalesForceAPI():
         if datetime.datetime.now() - self.settime > datetime.timedelta(minutes=5):
             self.tokenReset()
         # query = 'SELECT Name__c,Address1__c,Section_Name__c,Name FROM Customer_Data1__c WHERE Name__c LIKE \'東海カラー%\''
-        url = self.instance_url + "/services/data/v54.0/sobjects/" + sObject + "/"
-        headers = {'Authorization': self.token_type + ' ' + self.access_token}
+        # url = self.instance_url + self.url + "/sobjects/" + sObject + "/"
+        # headers = {'Authorization': self.token_type + ' ' + self.access_token}
         response = requests.get(
-            url,
-            headers=headers
+            self.instance_url + self.url + "/sobjects/" + sObject + "/",
+            headers={'Authorization': self.token_type + ' ' + self.access_token}
         )
         res = response.text
         return res
 
+    def version(self):
+        # url = self.instance_url + "/services/data" 
+        response = requests.get(
+            self.instance_url + "/services/data",
+        )
+        res = response.text
+        return res
+
+    def getResourse(self):
+        if datetime.datetime.now() - self.settime > datetime.timedelta(minutes=5):
+            self.tokenReset()
+        # query = 'SELECT Name__c,Address1__c,Section_Name__c,Name FROM Customer_Data1__c WHERE Name__c LIKE \'東海カラー%\''
+        # url = self.instance_url + self.url
+        # headers = {'Authorization': self.token_type + ' ' + self.access_token}
+        response = requests.get(
+            self.instance_url + self.url,
+            headers={'Authorization': self.token_type + ' ' + self.access_token}
+        )
+        res = response.text
+        return res
+
+    def getObjectList(self):
+            if datetime.datetime.now() - self.settime > datetime.timedelta(minutes=5):
+                self.tokenReset()
+  
+            response = requests.get(
+                self.instance_url + self.url + "/sobjects/",
+                headers={'Authorization': self.token_type + ' ' + self.access_token,
+                'X-PrettyPrint':'1'}
+            )
+            res = response.text
+            return res
+
+    def getObjecInfo(self, sObject="Customer_Data1__c"):
+            if datetime.datetime.now() - self.settime > datetime.timedelta(minutes=5):
+                self.tokenReset()
+  
+            response = requests.get(
+                self.instance_url + self.url + "/sobjects/" + sObject + "/",
+                headers={'Authorization': self.token_type + ' ' + self.access_token,
+                'X-PrettyPrint':'1'}
+            )
+            res = response.text
+            return res
+
+
 if __name__ == '__main__':
     sf = SalesForceAPI()
     print(sf.access_token)
+
+# APIのバージョンを取得
+    res = sf.version()
+    data = json.loads(res)
+    n = len(data)
+    sf.url = data[n-2]["url"]   # 最新のひとつ前のバージョンを指定
+    print(sf.url)
+
+    res = sf.getResourse()
+    sf.resourse = json.loads(res)
+    print(json.dumps(data, indent=4))
+
+    res = sf.getObjectList()
+    # print(res)
+    data = json.loads(res)
+    print(json.dumps(data, indent=4))
+
+    res = sf.getObjecInfo("Customer_Data1__c")
+    # print(res)
+    data = json.loads(res)
+    print(json.dumps(data, indent=4))
+
     res = sf.mataDataRead("Customer_Data1__c")
     # print(res)
     data = json.loads(res)
-    print(json.dumps(data, indent=2))
+    print(json.dumps(sf.resourse, indent=4))
 
     res = sf.dataread()
     print(res)
